5e29451608f0b97dcb393164df156b2af07e667a
Matt McMeans <mrmcmans@memphis.edu>
2023-11-25 18:10:29 -0600

    changed root path to home page

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,11 @@ Rails.application.routes.draw do
   #get '/about', to: 'pages#about', as: 'about'
   #get '/contact', to: 'pages#contact', as: 'contact'
 # get cat
-get 'store/cat', to: 'store#cat', as: 'cat'
+root 'store#cat'
+  get 'store/cat', to: 'store#cat', as: 'cat'
 #  get 'personal_information/show'
 
-  root 'pages#home'
+
   get '/home', to: 'pages#home', as: 'home'
   get 'items', to: 'items#index', as: 'items'
   get 'items/:id', to: 'items#show', as: 'item'
